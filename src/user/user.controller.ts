4244eb60135b1f6995334aeae578f007bb6e89c8
kittypitty <samutic40@gmail.com>
2023-04-03 12:48:01 +0300

    init: init Nest project with Prisma, add docker-compose for Postgres

---
+++ src/user/user.controller.ts
@@ -0,0 +1,17 @@
+import {Body, Controller, Post} from '@nestjs/common';
+import {User as UserModel} from "@prisma/client";
+import {UserService} from "./user.service";
+
+@Controller('user')
+export class UserController {
+    constructor(
+        private readonly userService: UserService,
+    ) {}
+
+    @Post('user')
+    async signupUser(
+        @Body() userData: { name?: string; email: string },
+    ): Promise<UserModel> {
+        return this.userService.createUser(userData);
+    }
+}
